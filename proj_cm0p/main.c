0ad95f873550f4453017b6a35a6c4c36873e284a
gitlab-runner <do-not-reply@cypress.com>
2022-09-22 05:14:36 -0500

    Upload mtb-example-xmc7000-switching-power-modes 1.0.0.339

---
+++ proj_cm0p/main.c
@@ -0,0 +1,96 @@
+/******************************************************************************
+* File Name:   main.c
+*
+* Description: This is the source code for CM0+ in XMC7000 MCU switching between 
+*              power modes application for ModusToolbox.
+*
+* Related Document: See Readme.md
+*
+*
+*******************************************************************************
+* Copyright 2022, Cypress Semiconductor Corporation (an Infineon company) or
+* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
+*
+* This software, including source code, documentation and related
+* materials ("Software") is owned by Cypress Semiconductor Corporation
+* or one of its affiliates ("Cypress") and is protected by and subject to
+* worldwide patent protection (United States and foreign),
+* United States copyright laws and international treaty provisions.
+* Therefore, you may use this Software only as provided in the license
+* agreement accompanying the software package from which you
+* obtained this Software ("EULA").
+* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+* non-transferable license to copy, modify, and compile the Software
+* source code solely for use in connection with Cypress's
+* integrated circuit products.  Any reproduction, modification, translation,
+* compilation, or representation of this Software except as specified
+* above is prohibited without the express written permission of Cypress.
+*
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+* reserves the right to make changes to the Software without notice. Cypress
+* does not assume any liability arising out of the application or use of the
+* Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use in any products where a malfunction or
+* failure of the Cypress product may reasonably be expected to result in
+* significant property damage, injury or death ("High Risk Product"). By
+* including Cypress's product in a High Risk Product, the manufacturer
+* of such system or application assumes all risk of such use and in doing
+* so agrees to indemnify Cypress against all liability.
+*******************************************************************************/
+
+#include "cy_pdl.h"
+
+/*******************************************************************************
+* Macros
+********************************************************************************/
+#define CM7_DUAL    1
+
+
+/*******************************************************************************
+* Function Name: main
+********************************************************************************
+* Summary:
+* This is the main function for the CM0P core. It does...
+*    1. Enable interrupts.
+*    2. Enable the CM7_0 core and/or CM7_1 core, according to macro CM7_DUAL setting.
+*    Do Forever loop:
+*    2. Put the CM0P core into DeepSleep mode.
+*
+* Parameters:
+*  void
+*
+* Return:
+*  int
+*
+*******************************************************************************/
+int main(void)
+{
+    /* enable interrupts */
+    __enable_irq();
+
+    /* Enable CM7_0/1. CY_CORTEX_M7_APPL_ADDR is calculated in linker script, check it in case of problems. */
+    Cy_SysEnableCM7(CORE_CM7_0, CY_CORTEX_M7_0_APPL_ADDR);
+
+    /* Wait for the clocks initialization to complete, to avoid race between CM7's.
+     * NOTE: the user application can also synchronize this using IPC or other way. 
+     */
+    Cy_SysLib_Delay(1000);
+
+#if CM7_DUAL
+    Cy_SysEnableCM7(CORE_CM7_1, CY_CORTEX_M7_1_APPL_ADDR);
+#endif /* CM7_DUAL */
+
+    /* Gets core clock frequency and updates SystemCoreClock */
+    SystemCoreClockUpdate();
+
+    Cy_SysPm_CpuSleepOnExit(true);
+
+    for(;;)
+    {
+        Cy_SysPm_CpuEnterDeepSleep(CY_SYSPM_WAIT_FOR_INTERRUPT);
+    }
+}
+
+/* [] END OF FILE */
